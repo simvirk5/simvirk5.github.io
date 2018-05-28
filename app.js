032917e733304d3f55179476aa0c9c3799bbba85
simvirk5 <simvirk5@gmail.com>
2018-05-28 15:17:44 -0700

    initial progress on landing page

---
+++ app.js
@@ -0,0 +1,14 @@
+$("#rotate")
+	.mouseover(function(){
+		$(this).toggleClass("rotated");
+})
+$("#rotate-right").mouseover(function(){
+	$(this).toggleClass("rotated");
+})
+$("#rotate-l").mouseover(function(){
+	$(this).toggleClass("rotated");
+})
+$("#rotate-left2").mouseover(function(){
+	$(this).toggleClass("rotated");
+})
+
