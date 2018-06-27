50213b04079e8d608e9a3f7f38817a81962ec454
simvirk5 <simvirk5@gmail.com>
2018-06-26 23:56:39 -0700

    updates to UI design

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
-$("#rotate")
-	.mouseover(function(){
-		$(this).toggleClass("rotated");
-})
-$("#rotate-right").mouseover(function(){
-	$(this).toggleClass("rotated");
-})
-$("#rotate-l").mouseover(function(){
-	$(this).toggleClass("rotated");
-})
-$("#rotate-left2").mouseover(function(){
-	$(this).toggleClass("rotated");
-})
+  $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
 
-// $(".about-text").click(function() {
-//     $('html,body').animate({
-//         scrollTop: $(".about").offset().top},
-//         'slow');
-// });
- $(document).ready(function(){
-    $('.modal').modal();
-  });
-
- $(window).bind('scroll', function() {
-     if ($(window).scrollTop() > 100) {
-         $('.disappear').hide();
-     }
-     else {
-         $('.disappear').show();
-     }
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
 });
+
+  $(document).ready(function(){
+    $('.modal').modal();
+    $('.parallax').parallax();
+  });
+    
